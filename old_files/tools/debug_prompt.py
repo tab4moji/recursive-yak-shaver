515801744562fb541f4837a9e08b3d7753ef7469
tab4moji <tab4moji@gmail.com>
2026-02-24 13:35:54 +0900

    save

--- old_files/tools/debug_prompt.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-from rys.role_utils import construct_system_prompt
-
-base_dir = os.getcwd()
-prompt = construct_system_prompt(base_dir, "coder", ["shell_exec"], True, None)
-print(prompt)
